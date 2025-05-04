bf18388617c7393b1fb6ad1f595087103a5e861a
Anderson <andersoncsilvadev@gmail.com>
2025-05-04 10:58:43 -0300

    criação do contator de like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,40 +1,50 @@
-import { ThumbsDown, Trash } from '@phosphor-icons/react'
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
-    
-    export function Comment({content, onDeleteComment}){
-        function handleDeleteComment(){
+import { useState } from 'react'
 
-            onDeleteComment(content)
-        }
-        return(
-            <div className= {styles.comment}>
-            <Avatar  hasBorder={false} src="https://files.tecnoblog.net/wp-content/uploads/2021/02/the-last-of-us-part-2-ellie-1060x596.jpg" alt="" />
-            
+export function Comment({ content, onDeleteComment }) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://files.tecnoblog.net/wp-content/uploads/2021/02/the-last-of-us-part-2-ellie-1060x596.jpg" alt="" />
+
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
-                <header>
-                        <div className= {styles.authorAndTime}>
+                    <header>
+                        <div className={styles.authorAndTime}>
                             <strong>Ellie</strong>
                             <time title="23 de Maio às 12:20" dateTime="2023-05-11">Cerca de 1 hora </time>
                         </div>
 
-                        <button onClick={handleDeleteComment} title ="Deletar comentario">
-                            <Trash size={20} />
+                        <button onClick={handleDeleteComment} title="Deletar comentario">
+                            <Trash size={24} />
                         </button>
-                </header>
+                    </header>
 
-                <p>{content}</p>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+
+                </footer>
             </div>
-
-            <footer>
-                <button>
-                    <ThumbsDown />
-                    Aplaudir <span>20</span>
-                </button>
-                
-            </footer>
         </div>
-    </div>
-        )
-    }
+    )
+}
